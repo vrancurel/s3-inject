a2753eb4f42868a64df8f49639016bfb318aad83
vrancurel <vianney.rancurel@gmail.com>
2018-02-16 20:04:54 +0000

    add a unique attribute

--- s3-inject.py
+++ s3-inject.py
@@ -4,9 +4,9 @@ session = boto3.session.Session()
 
 s3_client = session.client(
     service_name='s3',
-    aws_access_key_id='accessKey1',
-    aws_secret_access_key='verySecretKey1',
-    endpoint_url='http://127.0.0.1',
+    aws_access_key_id='IMD03TW256TY1R7HQTPY',
+    aws_secret_access_key='pVVCtoAht=UHK7OCqoi=BlUN2UWbsXFPOTxNNw/p',
+    endpoint_url='http://10.233.65.204:8001',
 )
 
 print(s3_client.list_buckets())
@@ -14,9 +14,10 @@ print(s3_client.list_buckets())
 counter = 0
 for counter in range(0,2000):
     name = '%s%s' % ('obj', counter)
+    attr = '%s%s' % ('bar', counter)
     print(name)
     s3_client.put_object(Bucket='foob3',
                          StorageClass='REDUCED_REDUNDANCY',
                          Key=name,
                          Body='this is a test',
-                         Metadata={ 'foo': 'bar' })
+                         Metadata={ 'foo': attr, 'bar': 'qux' })
