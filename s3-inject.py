48bc78d6b548e3e345aaa3c8109ec4cfc1c58ce1
vrancurel <vianney.rancurel@gmail.com>
2018-02-23 22:53:16 +0000

    add multiprocessing

--- s3-inject.py
+++ s3-inject.py
@@ -1,23 +1,25 @@
 import boto3
+import multiprocessing
 
-session = boto3.session.Session()
+def myfunc(i):
+    session = boto3.session.Session()
+    s3_client = session.client(
+        service_name='s3',
+        aws_access_key_id='SJC6KQDGRRLSQ9QJ0ODZ',
+        aws_secret_access_key='5EmgJJ4y2vJBKV3pS8t/l45kmaRkb4cdlBOE9NhC',
+        endpoint_url='http://10.233.99.137:8001',
+    )
+    counter = 0
+    for counter in xrange(0,1000000000):
+        name = '%s%s' % ('obj', counter + i)
+        attr = '%s%s' % ('bar', counter + i)
+        print(name)
+        s3_client.put_object(Bucket='foob8',
+                             StorageClass='REDUCED_REDUNDANCY',
+                             Key=name,
+                             Body='this is a test',
+                             Metadata={ 'foo': attr, 'bar': 'qux' })
 
-s3_client = session.client(
-    service_name='s3',
-    aws_access_key_id='IMD03TW256TY1R7HQTPY',
-    aws_secret_access_key='pVVCtoAht=UHK7OCqoi=BlUN2UWbsXFPOTxNNw/p',
-    endpoint_url='http://10.233.65.204:8001',
-)
-
-print(s3_client.list_buckets())
-
-counter = 0
-for counter in range(0,2000):
-    name = '%s%s' % ('obj', counter)
-    attr = '%s%s' % ('bar', counter)
-    print(name)
-    s3_client.put_object(Bucket='foob3',
-                         StorageClass='REDUCED_REDUNDANCY',
-                         Key=name,
-                         Body='this is a test',
-                         Metadata={ 'foo': attr, 'bar': 'qux' })
+for i in range(3):
+    t = multiprocessing.Process(target=myfunc, args=(i,))
+    t.start()
